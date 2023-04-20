424a09c08c39a218d4ed6b0293983a9b800cbd55
gnenefolo <samuel.soro@gmail.com>
2023-04-20 11:26:01 +0000

    Error fixed

--- demo-app/src/test/java/com/example/demoapp/DemoAppApplicationTests.java
+++ demo-app/src/test/java/com/example/demoapp/DemoAppApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demoapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoAppApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 }
